ea1a38b41c1e46e4c6ba7a579a59e284d2c64bbd
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-01-09 13:07:22 +0100

    fix: improve formatting in show_predictions_plt method

--- src/regression_logistique.py
+++ src/regression_logistique.py
@@ -65,23 +65,23 @@ class LogisticRegressionCustom:
         return np.mean(predictions == self.digits_target)
     
     def show_predictions_plt(self, num_samples=10):
-            sample_indices = np.random.choice(len(self.digits_data), num_samples, replace=False)
-            samples = self.digits_data[sample_indices]
-            true_labels = self.digits_target[sample_indices]
-            predicted_labels = self.predict(self.scaler.transform(samples))
-            
-            plt.figure(figsize=(10, 4))
-            for i, index in enumerate(sample_indices):
-                plt.subplot(2, num_samples // 2, i + 1)
-                if hasattr(self.digits, 'images'):  # For load_digits
-                    image = self.digits.images[index]
-                else:
-                    image = self.digits_data[index].reshape(self.image_shape)
-                plt.imshow(image, cmap='gray')
-                plt.title(f'Réalité: {true_labels[i]}\nPrédiction: {predicted_labels[i]}')
-                plt.axis('off')
-            plt.tight_layout()
-            plt.show()
+        sample_indices = np.random.choice(len(self.digits_data), num_samples, replace=False)
+        samples = self.digits_data[sample_indices]
+        true_labels = self.digits_target[sample_indices]
+        predicted_labels = self.predict(self.scaler.transform(samples))
+        
+        plt.figure(figsize=(10, 4))
+        for i, index in enumerate(sample_indices):
+            plt.subplot(2, num_samples // 2, i + 1)
+            if hasattr(self.digits, 'images'):  # For load_digits
+                image = self.digits.images[index]
+            else:
+                image = self.digits_data[index].reshape(self.image_shape)
+            plt.imshow(image, cmap='gray')
+            plt.title(f'Réalité: {true_labels[i]}\nPrédiction: {predicted_labels[i]}')
+            plt.axis('off')
+        plt.tight_layout()
+        plt.show()
     
     def confusion_matrix_display(self):
         predictions = self.predict(self.digits_data)
